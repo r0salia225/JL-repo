a87bb1d41d1a03570e39b279561b04a611c232b0
r0salia225 <jse64608@gmail.com>
2025-05-29 12:15:14 +0200

    Update zadanie_897.py
    dodane typy sortowania pierwsze babelkowe, drugie wstawiania a trzecie wyboru

--- 8_powtorzenie_z_wprowadzenia/zadanie_897.py
+++ 8_powtorzenie_z_wprowadzenia/zadanie_897.py
@@ -34,3 +34,64 @@ def najczestsza(slowo):
         tab[x] += 1
     m = 0
     for t in tab
+
+
+
+
+    tab = [3, 6, 7, 2, 8, 4, 1]
+print(tab)
+
+ile = 0
+n = len(tab)
+for i in range(len(tab)):
+    posortowane = True
+    for j in range(n-1):
+        ile += 1
+        if tab[j] > tab[j+1]:
+            tab[j], tab[j+1] = tab[j+1], tab[j]
+            posortowane = False
+        if posortowane:
+            break
+    n -= 1
+
+print(tab)
+print(ile)
+
+
+tab = [3, 2, 6, 1, 8, 2, 9]
+
+n = len(tab)
+i = n
+k = 0
+
+for j in range(n):
+    i = n - 1
+    t = tab[i]
+    while i > k:
+        if t < tab[i-1]:
+            tab[i] = tab[i - 1]
+        else:
+            t, tab[i] = tab[i - 1], t
+        i -= 1
+    tab[k] = t
+    k += 1
+
+print(tab)
+
+
+
+tab = [1, 9, 2, 8, 3, 7, 4, 5, 6]
+
+tab2 = []
+n = len(tab)
+for i in range(n):
+    I = 0
+    for j in range(len(tab)):
+        if tab[j] < tab[I]:
+            I = j
+
+    tab2.append(tab[I])
+    del tab[I]
+
+print(tab2)
+print(tab)
